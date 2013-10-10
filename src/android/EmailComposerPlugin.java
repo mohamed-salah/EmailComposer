2625e5bfab375bdb121eecce6c8cee8b281f84b5
Mohamed Salah <mohamed.salah@badrit.com>
2013-10-10 14:51:38 +0200

    fix

--- src/android/EmailComposerPlugin.java
+++ src/android/EmailComposerPlugin.java
@@ -25,7 +25,7 @@ import org.apache.cordova.CallbackContext;
 import org.apache.cordova.CordovaPlugin;
 import org.apache.cordova.api.LOG;
 
-public class EmailComposerPlugin extends CordovaPlugin {
+public class EmailComposer extends CordovaPlugin {
 
 	@Override
 	public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
